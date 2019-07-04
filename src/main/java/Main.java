ea16d970691785049539c4ffffd3b3ad5d049401
88143 <88143@farnborough.ac.uk>
2019-07-04 12:12:40 +0100

    Reading DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,50 @@
+import org.sqlite.SQLiteConfig;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Main {
+
+    public static Connection db = null;
+
+    public static void main(String[] args){
+        openDatabase("Database.db");
+        closeDatabase();
+    }
+
+    private static void openDatabase(String dbFile) {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
+            System.out.println("Database connection successfully established.");
+        } catch (Exception exception){
+            System.out.println("Database connection error: " + exception.getMessage());
+        }
+    }
+
+    private static void closeDatabase() {
+        try{
+            db.close();
+            System.out.println("Disconnected from database.");
+        } catch (Exception exception){
+            System.out.println("Database disconnection error: " + exception.getMessage());
+        }
+    }
+    public static void main(String[] args){
+        openDatabase("Database.db");
+        try{
+            PreparedStatement ps;
+            ps = db.prepareStatement("SELECT UserID, Username, DOB FROM Users");
+            ResultSet results = ps.executeQuery();
+            while(results.next()){
+                
+            }
+        }
+
+        closeDatabase();
+    }
+
 }
